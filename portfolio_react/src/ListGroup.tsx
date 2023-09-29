b8cbac0c84e30dc42811e0b05fd9576e1f3df6a2
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2023-09-28 21:21:49 -0700

    added list group function to be used globally

---
+++ portfolio_react/src/ListGroup.tsx
@@ -0,0 +1,13 @@
+//created list group function
+function ListGroup() {
+  return;
+  <ul className="list-group">
+    <li className="list-group-item">An item</li>
+    <li className="list-group-item">A second item</li>
+    <li className="list-group-item">A third item</li>
+    <li className="list-group-item">A fourth item</li>
+    <li className="list-group-item">And a fifth one</li>
+  </ul>;
+}
+// exported function to be used globally in the application
+export default ListGroup;
