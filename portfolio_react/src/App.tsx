3be3a0da0bf07cbdca62cd2e6342354653af72b0
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2023-09-28 21:28:31 -0700

    added ListGroup file to application
    added List Group file to application to be used later

--- portfolio_react/src/App.tsx
+++ portfolio_react/src/App.tsx
@@ -3,11 +3,14 @@
 //-----------------------
 //import 'Message.tsx for use in file
 import Message from "./Message";
+import ListGroup from "./ListGroup";
 
+// App function displays on the main page, adding diiv elements to display
 function App() {
   return (
     <div>
       <Message />
+      <ListGroup />
     </div>
   );
 }
