40165b3a8869f31563f3a0e22312e4da5106eb26
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2023-09-28 21:11:43 -0700

    Added message file
    Added a message file to the folder to be used globally in the application and removed un--necessary lines of code in the app file.

---
+++ portfolio_react/src/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing
+function Message() {
+  // JSX: JavaScript XML
+  const name = "Zachary Gibson";
+  return <h1>{name}</h1>;
+}
+
+export default Message; //exporting the file to be used globally through the application
