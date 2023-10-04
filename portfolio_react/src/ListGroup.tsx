b86e959af08f6089c05cc11f388775594d1f6496
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2023-10-03 22:34:59 -0700

    added dynamic functionality to the list group file

--- portfolio_react/src/ListGroup.tsx
+++ portfolio_react/src/ListGroup.tsx
@@ -1,13 +1,32 @@
+import { MouseEvent } from "react";
 //created list group function
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // must be wrapped in a single <div> element in order to render with react, or
+    // wrapped in empty brackets '<>' as a 'fragment'
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      {/* above line of code to render a message given there are no items available in the list */}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+        {/* using .map function to convert the list of 'items' to a differnt type. In this case, converting them into <li> elements */}
+        {/* the map function must be wrapped in curly braces for JSX markup */}
+        {/* additionally 'key' must be used to properly identify the item being rendered */}
+      </ul>
+    </>
   );
 }
 // exported function to be used globally in the application
